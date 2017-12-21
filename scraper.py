93c3344112829370c944c55d22b2b653a3c387a6
vlakos <vkostic86@gmail.com>
2017-12-22 00:01:49 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,12 +17,12 @@ print html
 # We use lxml, which is a Python library especially for parsing html.
 # -----------------------------------------------------------------------------
 
-#import lxml.html
-#root = lxml.html.fromstring(html) # turn our HTML into an lxml object
-#tds = root.cssselect('td') # get all the <td> tags
-#for td in tds:
-#    print lxml.html.tostring(td) # the full HTML tag
-#    print td.text                # just the text inside the HTML tag
+import lxml.html
+root = lxml.html.fromstring(html) # turn our HTML into an lxml object
+tds = root.cssselect('td') # get all the <td> tags
+for td in tds:
+    print lxml.html.tostring(td) # the full HTML tag
+    print td.text                # just the text inside the HTML tag
 
 # -----------------------------------------------------------------------------
 # 2. Save the data in the ScraperWiki datastore.
